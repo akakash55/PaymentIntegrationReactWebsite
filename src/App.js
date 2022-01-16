ca6032a7d5861de9fc8c47a97e3e30125eb94568
Akash Kumar <akakashkumarak55@gmail.com>
2022-01-16 09:41:55 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
+import { Switch, Route, Redirect } from 'react-router-dom';
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle";
+
+import Navbar from './Components/Navbar/Navbar';
+import Homepage from './Components/HomePage/Homepage';
+import Donate from './Components/Donate/Donate';
+import Contact from './Components/Contact/Contact';
+import Mission from './Components/Missions/Mission';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar />
+      <Switch>
+        <Route exact path="/" component={Homepage} />
+        <Route exact path="/donate" component={Donate} />
+        <Route exact path="/contact" component={Contact} />
+        <Route exact path="/mission" component={Mission} />
+        <Redirect to="/" />
+      </Switch>
+    </>
   );
 }
 
